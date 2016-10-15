b3a5b8bd9ed8c1c0448b189cdfd4007a291a5ae0
Marc McDougall <marcjmcdougall@gmail.com>
2016-10-15 00:27:31 -0400

    First iteration.  Baseline functionality in place!  Looks quite sexy.

--- assets/js/controllers/mainController.js
+++ assets/js/controllers/mainController.js
@@ -8,20 +8,41 @@ app.controller('MainController', ['$scope', function($scope) {
 
 			'id' : '0',
 			'content' : 'This is my first note, see!',
-			'priority' : 3
+			'priority' : 1
+		},
+
+		{
+
+			'id' : '0',
+			'content' : 'This is my second note, see!',
+			'priority' : 1
+		},
+
+		{
+
+			'id' : '0',
+			'content' : 'This is my third note, see!',
+			'priority' : 1
 		},
 
 		{
 
 			'id' : '1',
-			'content' : 'Ut non enim eleifend felis pretium feugiat. Aenean vulputate eleifend tellus. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Vivamus consectetuer hendrerit lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci.  Maecenas malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed aliquam ultrices mauris. Nulla sit amet est. Fusce vulputate eleifend sapien.',
-			'priority' : 3
+			'content' : 'Praesent ac massa at ligula laoreet iaculis.',
+			'priority' : 2
 		},
 		{
 
 			'id' : '2',
-			'content' : 'Phasellus gravida semper nisi. Donec venenatis vulputate lorem. Proin faucibus arcu quis ante. Morbi mattis ullamcorper velit.',
+			'content' : 'Praesent ac massa at ligula laoreet iaculis.',
 			'priority' : 3
 		}
 	];
+
+	$scope.remove = function(item){
+
+		var index = $scope.notes.indexOf(item);
+  		$scope.notes.splice(index, 1); 
+	};
+
 }]);
