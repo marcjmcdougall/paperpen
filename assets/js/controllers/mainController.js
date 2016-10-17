b7a738d727cb24c6693347763fdce57383207f23
Marc McDougall <marcjmcdougall@gmail.com>
2016-10-17 00:43:54 -0400

    Implemented new column layout structure, sorting and priority logic (and view).

--- assets/js/controllers/mainController.js
+++ assets/js/controllers/mainController.js
@@ -2,25 +2,11 @@ app.controller('MainController', ['$scope', 'notesService',  function($scope, no
 
 	$scope.notes = notesService.notes;
 
-	$scope.tempNoteTop = {
+	$scope.tempNote = {
 
 		'id' : 0,
 		'content' : '',
-		'priority' : 1
-	};
-
-	$scope.tempNoteMedium = {
-
-		'id' : 0,
-		'content' : '',
-		'priority' : 1
-	};
-
-	$scope.tempNoteLow = {
-
-		'id' : 0,
-		'content' : '',
-		'priority' : 1
+		'priority' : false
 	};
 
 	// This might be terribly inneficient.
@@ -46,32 +32,24 @@ app.controller('MainController', ['$scope', 'notesService',  function($scope, no
 			'priority' : priority
 		};
 
-		console.log('=====================');
-
-		console.log('content: ' + content + ", priority: " + priority);
-
-		console.log('Passing item now:');
-		console.log(newNote);
-
-		console.log('Scope item:');
-		console.log($scope.tempNoteTop);
-
 		// Pass it along to the service to manage.
 		notesService.add(newNote);	
-		resetTempNotes();
-		
+		resetTempNote();		
 	};
+
+	$scope.clear = function(){
+
+		$scope.notes = [];
+	}
 
 	var update = function(){
 
 		notesService.update($scope.notes);
 	}
 
-	var resetTempNotes = function(){
+	var resetTempNote = function(){
 
-		$scope.tempNoteTop.content = '';
-		$scope.tempNoteMedium.content = '';
-		$scope.tempNoteBottom.content = '';	
+		$scope.tempNote.content = '';
 	}
 
 }]);
